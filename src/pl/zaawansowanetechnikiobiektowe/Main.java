f052d14de5a199eacb34bcb778d883bbe60198e9
Damian <damianzorant@gmail.com>
2022-05-09 20:59:31 +0200

    zadanie00

--- src/pl/zaawansowanetechnikiobiektowe/Main.java
+++ src/pl/zaawansowanetechnikiobiektowe/Main.java
@@ -3,6 +3,6 @@ package pl.zaawansowanetechnikiobiektowe;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
--- src/pl/zaawansowanetechnikiobiektowe/Phone.java
+++ src/pl/zaawansowanetechnikiobiektowe/Phone.java
@@ -1,4 +1,5 @@
 package pl.zaawansowanetechnikiobiektowe;
 
 public class Phone {
+
 }
